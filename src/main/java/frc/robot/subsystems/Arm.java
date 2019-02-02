f152e59014577898dfca230821f914afe289bc9e
Richard-A-Wood <Rwood@nicoletfear.com>
2019-02-02 14:17:27 -0600

    update to Arm.

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,20 @@
+
+package frc.robot.subsystems;
+import  frc.robot.commands.*;
+import  frc.robot.RobotMap;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Arm extends Subsystem {
+	
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	// make sure the pistons are closed at first
+    public void initDefaultCommand() {
+ 
+    }
+}
