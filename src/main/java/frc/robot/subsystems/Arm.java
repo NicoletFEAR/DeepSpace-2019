a00be2267b450270b0cc2b11d32dad171b2a0df2
Richard-A-Wood <Rwood@nicoletfear.com>
2019-02-02 19:05:39 -0600

    Arm mech updated && Tested Updated some test commands in OI 6 levels for 6 targets on rockets PID System in Arm subsystem works, needs to be updated with PID values Will try and go back to desired position if kicked away. Cool stuff. MJ :/

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,20 +1,60 @@
 
 package frc.robot.subsystems;
 import  frc.robot.commands.*;
+import frc.robot.Robot;
 import  frc.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.SensorCollection;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- *
+ *This subsystem is used for precise positioning of the arm of the robot,
+ *and for extending/retracting the arm
  */
 public class Arm extends Subsystem {
-	
+    double p=.001;
+    double i=0;
+    double d=0;
+    double integral = 0;
+    double previousError = 0;
+    double previousDesiredEncoderValue = -10;
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	// make sure the pistons are closed at first
     public void initDefaultCommand() {
  
+    }
+
+    public boolean rotateToPosition(int desiredEncoderValue){
+        //checks if the target has changed
+        //if it has changed, reset the base variables to 0;
+        if(desiredEncoderValue!=previousDesiredEncoderValue){
+            integral=0;
+            previousError=0;
+            previousDesiredEncoderValue = desiredEncoderValue;
+            RobotMap.armMotor1.setSelectedSensorPosition(0,0,10);
+        }
+        //reflecting it across the center of the robot if it's reversed
+        if(Robot.driveTrain.isReversed()){
+            // desiredEncoderValue+= 2*(RobotMap.ARM_MAX_TICK_VAL/2-desiredEncoderValue);
+            desiredEncoderValue = (int)RobotMap.ARM_MAX_TICK_VAL-desiredEncoderValue;
+        }
+        SensorCollection sensor = RobotMap.armMotor1.getSensorCollection();
+
+        SmartDashboard.putNumber("Arm Encoder Value", sensor.getQuadraturePosition());
+
+        double error = desiredEncoderValue + sensor.getQuadraturePosition();
+        integral += error*.02;
+        double derivative = (error-previousError)/.02;
+        double speed = p*error + i*integral + d*derivative;
+        RobotMap.armMotor1.set(ControlMode.PercentOutput, speed);
+        RobotMap.armMotor2.set(ControlMode.PercentOutput, -speed);
+        return (0!=Robot.oi.getXbox1().getTriggerAxis(Hand.kLeft)) || (0!=Robot.oi.getXbox1().getTriggerAxis(Hand.kRight));
     }
 }
