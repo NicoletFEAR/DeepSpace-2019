fc3cb6ae0eb1282e39eafb02df03a238379cd65b
Richard-A-Wood <Rwood@nicoletfear.com>
2019-02-05 20:53:07 -0600

    Forgot to add this one. All the best. MJ :/

--- src/main/java/frc/robot/commands/CargoLevel2.java
+++ src/main/java/frc/robot/commands/CargoLevel2.java
@@ -13,6 +13,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Robot;
+import frc.robot.RobotMap;
 
 
 /**
