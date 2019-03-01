8e7a3aa64de045921fa9debb204d4a33fc511070
fear6 <lawoodwi@gmail.com>
2019-02-28 19:14:54 -0600

    Code format and add javadocs to CompressAir subsystem

--- src/main/java/frc/robot/subsystems/CompressAir.java
+++ src/main/java/frc/robot/subsystems/CompressAir.java
@@ -12,15 +12,24 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
 /**
- * Subsystem representing the compressor
+ * Subsystem representing the compressor on the robot
  */
 public class CompressAir extends Subsystem {
   private Compressor theCompressor;
 
+  /**
+   * Initialize the compressor on the robot
+   */
   public CompressAir() {
     theCompressor = new Compressor(RobotMap.compressormodule);
   }
 
+  /**
+   * Toggles the compressor on and forced off. If forced off, the compressor will
+   * not run no matter what. (Obviously! :D) If on, the compressor will turn on
+   * and off depending on the reading of a pressure sensor connected to the
+   * Pneumatic Control Module.
+   */
   public void toggleCompressor() {
     if (theCompressor.enabled() == false) {
       theCompressor.start();
@@ -29,10 +38,19 @@ public class CompressAir extends Subsystem {
     }
   }
 
+  /**
+   * Reports whether the compressor is on or off.
+   * 
+   * @return is the compressor enabled
+   */
   public boolean isEnabled() {
     return theCompressor.enabled();
   }
 
+  /**
+   * Usually species a default command to run when the subsystem starts up, but
+   * does nothing in this subsystem.
+   */
   @Override
   public void initDefaultCommand() {
   }
